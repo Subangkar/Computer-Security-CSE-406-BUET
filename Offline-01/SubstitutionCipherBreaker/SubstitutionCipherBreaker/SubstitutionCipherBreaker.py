901ef217a864bd82651bfe2afe5678978b56dc52
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-09 22:46:30 +0600

    possible ones detected

--- Offline-01/SubstitutionCipherBreaker/SubstitutionCipherBreaker/SubstitutionCipherBreaker.py
+++ Offline-01/SubstitutionCipherBreaker/SubstitutionCipherBreaker/SubstitutionCipherBreaker.py
@@ -8,11 +8,23 @@ orig_value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 cipher_text = "THQVHAQCYCSPMAWARNTLNRTAVQUOEQWWNPQRTZQEYAWARTHQTENARTHQTENARKALLONWWMFINOQWTNTCAXQINKRVEQNTQTHQNXNLNRVHQKHQRTHQTENARLQNXQWTHQWTNTAYR "
 clear_text = cipher_text
 freq_chars = ['e', 't', 'a']
-freq_words = ['atlantic', 'express', 'avalanche', 'station']
+freq_words = ['atlantic', 'express', 'avalanche', 'station',
+              'and',
+              'ing',
+              'of'
+              # 'be',
+              # 'by',
+              ]
 freq_pattern = {'atlantic': 'at[A-Z]a[A-Z]t[A-Z][A-Z]'
 	, 'express': re.compile(r'e[A-Z]{3}e([A-Z])\1')
 	, 'avalanche': 'a[A-Z]alanc[A-Z]e'
-	, 'station': 'stati[A-Z]n'}
+	, 'station': 'stati[A-Z]n'
+	, 'and': 'an[A-Z]'
+	, 'ing': 'in[A-Z]'
+	, 'of': 'o[A-Z]'
+                # , 'be': '[A-Z]e'
+                # , 'by': 'b[A-Z]'
+                }
 
 # ------------------ Map & Update --------------------
 key_map = dict()
@@ -24,15 +36,12 @@ def updateMap(cipher, clear, key):
 	for i in range(cipher.__len__()):
 		if cipher[i] != clear[i] and clear[i].islower():
 			if cipher[i].islower() or clear[i].isupper() or key[cipher[i]] != '\0':
-				print("error > ", end=" ")
-				print("cipher="+cipher[i], end=" ")
-				print("clear="+clear[i], end=" ")
-				print("key="+key[cipher[i]], end="\n")
+				print("redundant > ", end=" ")
+				print("cipher=" + cipher[i], end=" ")
+				print("clear=" + clear[i], end=" ")
+				print("key=" + key[cipher[i]], end="\n")
 			else:
 				key[cipher[i]] = clear[i]
-
-
-# return key
 
 
 def updatedClearTextWithMap(clear, key):
@@ -41,6 +50,17 @@ def updatedClearTextWithMap(clear, key):
 			clear = clear.replace(char, key[char])
 	return clear
 
+
+def printkey(key):
+	print(orig_value)
+	count=0
+	for c in orig_value:
+		if key[c] != '\0':
+			print(key[c], end="")
+		else:
+			count+=1
+			print(' ', end="")
+	print("\n"+str(count)+' Undetected')
 
 # ------------------ Most Freq Chars Replacement --------------------
 print(clear_text)  # Cipher
@@ -66,11 +86,10 @@ for i in range(freq_words.__len__()):
 	clear_text = updatedClearTextWithMap(clear_text, key_map)
 	print(clear_text)  # word 1 replaced
 
-# s = re.sub('at[A-Z]a[A-Z]t[A-Z][A-Z]', freq_words[0], clear_text)
-# updateMap(clear_text, s, key_map)
-# print(s)  # frequent Replaced
-# clear_text = updatedClearTextWithMap(clear_text, key_map)
-# print(clear_text)  # word 1 replaced
-
+print("Key: ")
+printkey(key_map)
+print("Cipher: "+cipher_text)
+print("Clear : "+clear_text)
+# printkey(key_map)
 # s = "helloworld"
 # print(collections.Counter(s).most_common(1)[0])
