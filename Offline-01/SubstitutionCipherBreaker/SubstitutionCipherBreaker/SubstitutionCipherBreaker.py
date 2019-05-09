fe2e8f27ab756f2c6a8313427874ad31fbb1ffae
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-09 20:55:37 +0600

    single char replaced

--- Offline-01/SubstitutionCipherBreaker/SubstitutionCipherBreaker/SubstitutionCipherBreaker.py
+++ Offline-01/SubstitutionCipherBreaker/SubstitutionCipherBreaker/SubstitutionCipherBreaker.py
@@ -1,6 +1,34 @@
-cipher_text = ""
-clear_text = cipher_text
+import secondMostChar
 
-import collections
-s = "helloworld"
-print(collections.Counter(s).most_common(1)[0])
+# ------------------ Data --------------------
+orig_value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# ------------------ INPUT --------------------
+cipher_text = "THQVHAQCYCSPMAWARNTLNRTAVQUOEQWWNPQRTZQEYAWARTHQTENARTHQTENARKALLONWWMFINOQWTNTCAXQINKRVEQNTQTHQNXNLNRVHQKHQRTHQTENARLQNXQWTHQWTNTAYR "
+clear_text = cipher_text
+freq_chars = ['e', 't', 'a']
+freq_words = ['atlantic', 'express', 'avalanche', 'station']
+
+# ------------------ Map --------------------
+key_map = dict()
+for char in orig_value:
+	key_map[char] = '\0'
+
+# ------------------ Most Freq Chars Replacement --------------------
+list_chars = (secondMostChar.char_count(cipher_text))
+
+for i in range(freq_chars.__len__()):
+	print(freq_chars[i], end=' > ')
+	print(list_chars[-i - 1][0], end=' ')
+	print(list_chars[-i - 1][1], end='\n')
+	key_map[list_chars[-i - 1][0]] = freq_chars[i]
+	clear_text = clear_text.replace(list_chars[-i - 1][0], key_map[list_chars[-i - 1][0]])
+
+print(cipher_text)
+print(clear_text)
+# ------------------ Word Replacement --------------------
+
+# import collections
+
+# s = "helloworld"
+# print(collections.Counter(s).most_common(1)[0])
