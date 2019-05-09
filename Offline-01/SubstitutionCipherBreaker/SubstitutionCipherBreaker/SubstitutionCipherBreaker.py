c546ae273d42b71fc0cc36b2192f89199718542f
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-09 23:26:16 +0600

    provided words & guessed ones separated

--- Offline-01/SubstitutionCipherBreaker/SubstitutionCipherBreaker/SubstitutionCipherBreaker.py
+++ Offline-01/SubstitutionCipherBreaker/SubstitutionCipherBreaker/SubstitutionCipherBreaker.py
@@ -7,24 +7,35 @@ orig_value = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 # ------------------ INPUT --------------------
 cipher_text = "THQVHAQCYCSPMAWARNTLNRTAVQUOEQWWNPQRTZQEYAWARTHQTENARTHQTENARKALLONWWMFINOQWTNTCAXQINKRVEQNTQTHQNXNLNRVHQKHQRTHQTENARLQNXQWTHQWTNTAYR "
 clear_text = cipher_text
+
 freq_chars = ['e', 't', 'a']
-freq_words = ['atlantic', 'express', 'avalanche', 'station',
-              'and',
-              'ing',
-              'of'
-              # 'be',
-              # 'by',
-              ]
-freq_pattern = {'atlantic': 'at[A-Z]a[A-Z]t[A-Z][A-Z]'
+
+freq_words = ['atlantic', 'express', 'avalanche', 'station']
+freq_pattern = {'atlantic': 'at[A-Z]a[A-Z]t[A-Z]{2}'
 	, 'express': re.compile(r'e[A-Z]{3}e([A-Z])\1')
 	, 'avalanche': 'a[A-Z]alanc[A-Z]e'
 	, 'station': 'stati[A-Z]n'
-	, 'and': 'an[A-Z]'
-	, 'ing': 'in[A-Z]'
-	, 'of': 'o[A-Z]'
-                # , 'be': '[A-Z]e'
-                # , 'by': 'b[A-Z]'
                 }
+
+freq_words_custom = [
+	'and',
+	# 'ing',
+	'will',
+	'of',
+	# 'be',
+	# 'by',
+	'passby'
+]
+
+freq_pattern_custom = {
+	'and': 'an[A-Z]'
+	# , 'ing': 'in[A-Z]'
+	, 'will': '[A-Z]ill'
+	, 'of': 'o[A-Z]'
+	# , 'be': '[A-Z]e'
+	# , 'by': 'b[A-Z]'
+	, 'passby': 'pass[A-Z]{2}'
+}
 
 # ------------------ Map & Update --------------------
 key_map = dict()
@@ -52,31 +63,35 @@ def updatedClearTextWithMap(clear, key):
 
 
 def printkey(key):
-	print(orig_value)
-	count=0
+	for c in orig_value:
+		print(c, end=" ")
+	print()
+
+	count = 0
 	for c in orig_value:
 		if key[c] != '\0':
-			print(key[c], end="")
+			print(key[c], end=" ")
 		else:
-			count+=1
-			print(' ', end="")
-	print("\n"+str(count)+' Undetected')
+			count += 1
+			print(' ', end=" ")
+	print("\n" + str(count) + ' Undetected')
+
 
 # ------------------ Most Freq Chars Replacement --------------------
 print(clear_text)  # Cipher
 
-list_chars = (secondMostChar.char_count(cipher_text))
+freq_count_list = (secondMostChar.char_count(cipher_text))
 
 for i in range(freq_chars.__len__()):
 	# print(freq_chars[i], end=' > ')
-	# print(list_chars[-i - 1][0], end=' ')
-	# print(list_chars[-i - 1][1], end='\n')
-	key_map[list_chars[-i - 1][0]] = freq_chars[i]
-	clear_text = clear_text.replace(list_chars[-i - 1][0], key_map[list_chars[-i - 1][0]])
+	# print(freq_count_list[-i - 1][0], end=' ')
+	# print(freq_count_list[-i - 1][1], end='\n')
+	key_map[freq_count_list[-i - 1][0]] = freq_chars[i]
+	clear_text = clear_text.replace(freq_count_list[-i - 1][0], key_map[freq_count_list[-i - 1][0]])
 
 print(clear_text)  # frequent Replaced
 
-# ------------------ Word Replacement --------------------
+# ------------------ Word Replacement Provided --------------------
 
 # print(freq_pattern[freq_words[0]])
 for i in range(freq_words.__len__()):
@@ -86,10 +101,28 @@ for i in range(freq_words.__len__()):
 	clear_text = updatedClearTextWithMap(clear_text, key_map)
 	print(clear_text)  # word 1 replaced
 
+print("\n --------- From Provided Words Only ---------- ")
 print("Key: ")
 printkey(key_map)
-print("Cipher: "+cipher_text)
-print("Clear : "+clear_text)
+print("Cipher: " + cipher_text)
+print("Clear : " + clear_text)
+
+# ------------------ Word Replacement Custom --------------------
+
+# print(freq_pattern[freq_words[0]])
+for i in range(freq_words_custom.__len__()):
+	s = re.sub(freq_pattern_custom[freq_words_custom[i]], freq_words_custom[i], clear_text)
+	print(s)  # frequent Replaced
+	updateMap(clear_text, s, key_map)
+	clear_text = updatedClearTextWithMap(clear_text, key_map)
+	print(clear_text)  # word 1 replaced
+
+print("\n --------- From Visible Intuition ---------- ")
+print("Key: ")
+printkey(key_map)
+print("Cipher: " + cipher_text)
+print("Clear : " + clear_text)
+
 # printkey(key_map)
 # s = "helloworld"
 # print(collections.Counter(s).most_common(1)[0])
