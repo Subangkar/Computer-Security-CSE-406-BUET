baa8a608da4190ee612b711bf07ca24270fafa5e
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-05-11 12:19:47 +0600

    des done

--- Offline-01/DES/des.py
+++ Offline-01/DES/des.py
@@ -1,19 +1,20 @@
 import bitstring
 import data
 
+ENCODE = 1
+DECODE = 0
+
 
 class Des:
 	def __init__(self):
 		self.password = None
 		self.text = None
-		self.keys = list()
 		self.pad_len = 0
+		self.keys = list()
 
-	def encrypt(self, key, text):
+	def encdec(self, key, text, action):
 		self.text = text
 		self.password = key
-
-		self.addPadding()
 
 		self.generatekeys()  # Generate all the keys for 16 iterations
 		text_blocks = bitstring.nsplit(self.text, 8)  # Split the text in blocks of 8 bytes so 64 bits
@@ -25,26 +26,19 @@ class Des:
 			tmp = None
 			for i in range(16):  # Do the 16 rounds
 				d_e = self.expand(d, data.E)  # Expand d to match Ki size (48bits)
-				tmp = self.xor(self.keys[i], d_e)  # If encrypt use Ki
-				# tmp = self.substitute(tmp)  # Method that will apply the SBOXes
+				if action == ENCODE:
+					tmp = self.xor(self.keys[i], d_e)  # If encrypt use Ki
+				else:
+					tmp = self.xor(self.keys[15 - i], d_e)  # If decrypt start by the last key
 				tmp = self.permut(tmp, data.P)
 				tmp = self.xor(g, tmp)
 				g = d
 				d = tmp
 			result += self.permut(d + g, data.PI_1)  # Do the last permut and append the result to result
+		self.text = bitstring.bit_array_to_string(result)
 
-		self.removePadding()
-
-	def addPadding(self):  # pad with \0.
-		if len(self.text) % 8 != 0:
-			self.pad_len = 8 - (len(self.text) % 8)
-		self.text += self.pad_len * '\0'
-
-	def removePadding(self):  # Remove the padding of the plain text (it assume there is padding)
-		if self.pad_len > 0:
-			self.text = self.text[:-self.pad_len]
-
-	def generatekeys(self):  # Algorithm that generates all the keys
+	# Initial x56 key and x48 key_i for each iterations
+	def generatekeys(self):
 		self.keys = []
 		key = bitstring.string_to_bit_array(self.password)
 		key = self.permut(key, data.CP_1)  # Apply the initial permut on the key
@@ -63,13 +57,42 @@ class Des:
 	def shift(self, g, d, n):  # Shift a list of the given value
 		return g[n:] + g[:n], d[n:] + d[:n]
 
+	def xor(self, t1, t2):  # Apply a xor and return the resulting list
+		return [x ^ y for x, y in zip(t1, t2)]
+
+	def encrypt(self, key, text):
+		self.encdec(key, text, ENCODE)
+		return self.text
+
+	def decrypt(self, key, text):
+		self.encdec(key, text, DECODE)
+		return self.text
+
+
+def addPadding(text):  # pad with \0.
+	pad_len = 0
+	if len(text) % 8 != 0:
+		pad_len = 8 - (len(text) % 8)
+	text += pad_len * '0'
+	return text, pad_len
+
+
+def removePadding(text, pad_len):  # Remove the padding of the plain text (it assume there is padding)
+	if pad_len > 0:
+		return text[:-pad_len]
+	else:
+		return text
+
 
 if __name__ == '__main__':
-	key = "megabuck"  # "secret_k"
-	text = "Hello world12345"
+	# key = "megabuck"
+	# text = "Hello world"
+	key = input("Enter Key: ")
+	text = input("Enter Plain Text: ")
+	plain_text, pad_len = addPadding(text)
 	d = Des()
-	r = d.encrypt(key, text)
-	# r2 = d.decrypt(key, r, True)
+	cipher_text = d.encrypt(key, plain_text)
+	decrypted_text = removePadding(d.decrypt(key, cipher_text), pad_len)
 	print("Key       : %r" % key)
-	print("Ciphered  : %r" % r)
-# print("Deciphered: %r" % r2)
+	print("Ciphered  : %r" % cipher_text)
+	print("Deciphered: %r" % decrypted_text)
