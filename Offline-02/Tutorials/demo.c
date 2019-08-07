0716abcdbe8d7f70d33d1a2c43eb6c188ffb5919
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-08-08 01:05:38 +0600

    demonstration manual

---
+++ Offline-02/Tutorials/demo.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int foo(char *str)
+{
+	char buffer[100];
+
+	strcpy(buffer, str);
+	return 1;
+}
+
+int main(int argc, char **argv)
+{
+	char str[400];
+	FILE *badfile;
+	
+	badfile = fopen("badfile","r");
+	fread(str, sizeof(char), 300, badfile);
+	foo(str);
+
+	printf("Returned Properly\n");
+	return 1;
+}
