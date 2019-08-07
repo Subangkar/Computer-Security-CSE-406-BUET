f26baa32555cd2066574a5b3ab937951dc110c2f
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-08-07 23:21:32 +0600

    helping codes practice

--- Offline-02/Tutorials/tom.c
+++ Offline-02/Tutorials/tom.c
@@ -4,5 +4,7 @@ int main(int argc, char **argv)
 {
     printf("Real user id is %d\n", getuid());
     printf("Effective user id is %d\n", geteuid());
+    printf("Real group id is %d\n", getgid());
+    printf("Effective group id is %d\n", getegid());
     return 0;
 }
