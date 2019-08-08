3c758c7b78e7e04b0951c58ad70a2550195d8b48
Subangkar-HP-Win10 <subangkar.karmaker@gmail.com>
2019-08-09 01:50:33 +0600

    attack done using zsh

--- Offline-02/buffer-overflow-attack/compile.sh
+++ Offline-02/buffer-overflow-attack/compile.sh
@@ -3,4 +3,5 @@ filename="$1"
 execname=`echo "$filename" | sed 's/\..*//'`
 gcc -o "$execname" -z execstack -fno-stack-protector "$filename"
 sudo chown root "$execname"
-sudo chmod 4755 "$execname"
+sudo chmod 4777 "$execname"
+#sudo chmod 4755 "$execname"
